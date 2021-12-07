705ff64e92f02637cd0a1304b98cc46c7f133780
NTT1906 <nttispay@gmail.com>
2021-12-07 19:02:24 +0700

    async :/?

--- src/arie/reddust/listener/ItemEntityListener.php
+++ src/arie/reddust/listener/ItemEntityListener.php
@@ -7,18 +7,24 @@ use pocketmine\entity\object\ItemEntity;
 use pocketmine\event\entity\ItemDespawnEvent;
 use pocketmine\event\entity\ItemSpawnEvent;
 use pocketmine\event\Listener;
-use pocketmine\world\Position;
-use pocketmine\world\World;
 
-use arie\reddust\block\Hopper;
 use arie\reddust\Loader;
 
 use muqsit\asynciterator\AsyncIterator;
 use muqsit\asynciterator\handler\AsyncForeachHandler;
 use muqsit\asynciterator\handler\AsyncForeachResult;
 
+use ArrayIterator;
+use LogicException;
+
 final class ItemEntityListener implements Listener{
-    private $entities = [];
+    /** @var AsyncIterator */
+    private AsyncIterator $async_iterator;
+
+    /** @var AsyncForeachHandler<int, ItemEntity>|null */
+    private ?AsyncForeachHandler $ticker;
+
+    private array $entities = [];
 
     public function __construct(
         private Loader $plugin,
@@ -63,13 +69,18 @@ final class ItemEntityListener implements Listener{
 
     public function registerItemEntity(ItemEntity $entity) : void{
         if (!$entity->isClosed() && !$entity->isFlaggedForDespawn()) {
-            $this->entities[$entity->getId()] = $entity->getId();
+            $this->entities[$entity->getId()] = $entity;
+            if ($this->ticker === null) $this->tick();
         }
     }
 
     public function deregisterItemEntity(ItemEntity $entity) {
         if (isset($this->entities[$id = $entity->getId()])) {
             unset($this->entities[$id]);
+            if ($this->ticker !== null && count($this->entities) === 0) {
+                $this->ticker->cancel();
+                $this->ticker = null;
+            }
         }
     }
 
@@ -84,5 +95,25 @@ final class ItemEntityListener implements Listener{
         $entity = $event->getEntity();
         if ($entity instanceof ItemEntity)
             $this->deregisterItemEntity($entity);
+    }
+
+    public function isTicking() : bool{
+        return $this->ticker !== null;
+    }
+
+    private function tick() : bool{
+        if($this->ticker !== null){
+            throw new LogicException("Tried scheduling multiple item entity tickers");
+        }
+        $this->ticker = $this->async_iterator->forEach(new ArrayIterator($this->entities), 1, 4)->as(static function(int $id, ItemEntity $entity) : AsyncForeachResult{
+            if(!$entity->isClosed() && !$entity->isFlaggedForDespawn()){
+                $this->onItemEntityMove($entity);
+            }
+            return AsyncForeachResult::CONTINUE();
+        })->onCompletion(function() : void{
+            $this->ticker = null;
+            $this->tick();
+        });
+        return true;
     }
 }
